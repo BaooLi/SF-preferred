bf657a78528555f65e6d422563219eaae10f6a71
zhufengzhufeng <@710868695>
2017-12-20 09:27:50 +0800

    commit

--- src/containers/Cart/CartUp/index.js
+++ src/containers/Cart/CartUp/index.js
@@ -1,53 +1,67 @@
-//
-// import React,{Component} from "react";
-// import './index.css';
-// import {Link} from 'react-router-dom';
-//
-// export default class CartShop extends Component{
-//     render(){
-//         return (
-//             <div className="cart-shop">
-//                 <div className="top">
-//                     <i className="iconfont icon-fanhui"></i>
-//                     <span className="fanhui">返回</span>
-//                     <i className="iconfont icon-icon-yxj-a"></i>
-//                     <span className="dizhi">北京市 北京</span>
-//                     <Link to="CartUp"><span className="qie bianji">编辑</span></Link>
-//
-//                 </div>
-//                 <div className="container">
-//                     <div className="better">
-//                         <i class="iconfont icon-duihao3"></i>
-//                         <h5>优选普通商品</h5>
-//                     </div>
-//
-//                     <ul className='products'>
-//                         <li className="product">
-//                             <i className="iconfont icon-duihao3"></i>
-//                             <img src={require("../images/shiwu.jpg")} alt=""/>
-//
-//                                 <i className="iconfont icon-shoucang1"></i>
-//                                 <i className="iconfont icon-shanchu"></i>
-//                                 <span>-</span>2<span>+</span>
-//
-//
-//                         </li>
-//                     </ul>
-//                 </div>
-//                 <div className="wei">
-//                     <h5>——— 为你推荐 ———</h5>
-//                 </div>
-//                 <div className="shop-footer">
-//                     <i className="iconfont icon-duihao3"></i>
-//                     <span className="span1">总计:</span>
-//                     <span className="span2">￥</span>
-//                     <span className="span3">96.1</span>
-//                     <span className="span4">商品金额:</span><span    className="span5">￥96.1</span>
-//                     <span className="span6">(不含运费)</span>
-//                     <button>结算</button>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-//
+import React,{Component} from "react";
+import './index.css';
+import {Link} from 'react-router-dom';
+
+export default class CartUp extends Component{
+    render(){
+        return (
+            <div className="cart-up">
+
+
+                <div className="top">
+                    <i className="iconfont icon-fanhui"></i>
+                    <span className="dizhi">北京市 北京</span>
+                    <Link to="CartUp"><span className="bianji">完成</span></Link>
+                </div>
+
+
+                <div className="container">
+                    <div className="better">
+                        <i class="iconfont icon-duihao3"></i>
+                        <h5>优选普通商品</h5>
+                    </div>
+                    <ul className='products'>
+
+                        <li className="product">
+                            <div className="product-left">
+
+                                <i className="iconfont icon-duihao3"></i>
+                                <img src={require("../images/shiwu.jpg")} alt=""/>
+
+                            </div>
+
+                            <div className="detail">
+                               <i className="iconfont icon-shanchu"></i>
+                                <button className="sub">-</button><span>1</span><button className="add">+</button>
+
+                            </div>
+
+
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div className="wei">
+                    <h5>——— 为你推荐 ———</h5>
+                </div>
+
+
+                <div className="up-footer">
+                    <div className="footer-left">
+                        <i className="iconfont icon-duihao3"></i>
+                        <span className="span1">总计:</span>
+                        <span className="span2">￥96.1</span>
+                        <span className="span3">商品金额:</span>
+
+                        <span className="span4">￥96.1</span><span    className="span5">(不含运费)</span>
+
+                    </div>
+                    <button>批量删除</button>
+                </div>
+
+
+            </div>
+        )
+    }
+}
--- src/containers/Cart/index.js
+++ src/containers/Cart/index.js
@@ -1,14 +1,14 @@
 import React,{Component} from "react";
   // import CartNo from './CartNo/index';
- import CartShop from './CartShop/index';
- // import CartUp from './CartUp/index';
+ //import CartShop from './CartShop/index';
+  import CartUp from './CartUp/index';
 export default class Cart extends Component{
     render(){
         return(
             <div>
               {/*<CartNo/>*/}
-              <CartShop/>
-              {/*<CartUp/>*/}
+              {/*<CartShop/>*/}
+              <CartUp/>
             </div>
         )
     }
