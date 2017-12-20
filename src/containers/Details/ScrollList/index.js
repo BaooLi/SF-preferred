8aa8d8e8282e05f50783f1e62a2db65bd77cc9cf
sunxiaodan123 <1679922628@qq.com>
2017-12-20 18:33:16 +0800

    提交上拉跳转

--- src/containers/Details/ScrollList/index.js
+++ src/containers/Details/ScrollList/index.js
@@ -2,17 +2,24 @@ import React, {Component} from 'react'
 export default class ScrollList extends Component {
     componentWillReceiveProps(nextProps) {
         if (nextProps.element) {
-            nextProps.element.addEventListener('scroll', () => {
-                alert(1)
-                let {offsetHeight, scrollHeight, scrollTop} = nextProps.element
-                console.log(nextProps.element)
-                console.log(offsetHeight, scrollHeight, scrollTop);
+            let id=this.props.id
+            nextProps.element.addEventListener('scroll', (event) => {
+                this.timer?clearInterval(this.timer):null
+                this.timer=setTimeout(()=>{
+                    let {offsetHeight, scrollHeight, scrollTop} = nextProps.element
+                    if (offsetHeight + scrollTop + 100 > scrollHeight) {
+                        console.log('daole');
+                        nextProps.element.addEventListener('touchend',()=>{
+                            console.log(2);
+                            this.props.history.push(`/pictureTextDetail/${id}`)
+                        })
+                    }
+                },12)
             })
         }
     }
 
     render() {
-
         return (
             <div>
                 {this.props.children}
@@ -20,4 +27,5 @@ export default class ScrollList extends Component {
         )
     }
 }
+
 
