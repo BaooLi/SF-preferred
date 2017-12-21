d51bea7f5955707374f65aaaaa319b2edcdc5e53
sunxiaodan123 <1679922628@qq.com>
2017-12-21 13:37:16 +0800

    商品详情

---
+++ src/containers/List/Classification/index.js
@@ -0,0 +1,59 @@
+import React,{Component} from 'react';
+import Filter from '../Filter'
+import ListSearch from '../Listsearch'
+import {connect} from "react-redux"
+import actions from "../../../store/actions/list"
+import './index.less'
+import {Link} from 'react-router-dom'
+
+
+class Classification extends Component{
+    componentDidMount(){
+        this.props.fetchClassifications(this.props.match.params.title);
+        console.log(this.props.match.params.title);
+    }
+
+    render(){
+        let dataClassifications=this.props.classifications||[];
+        console.log(dataClassifications);
+        return(
+            <div className="productList">
+                <ListSearch/>
+                <Filter
+                    keyWord={this.props.match.params.title}
+                        fetchClassifications={this.props.fetchClassifications}
+                    type={this.props.type}
+                />
+                {/*商品列表*/}
+                    <ul className="product-list">
+                        {
+                            dataClassifications&&dataClassifications.map((item,index)=>(
+                                <li className="cations-item" key={index}>
+                                    <Link  to={{pathname:`/detail/${item.recommendID
+                                        }`}}  key={index} >
+                                        <div className="contentImg">
+                                            <img src={item.recommendImg} />
+                                        </div>
+                                        <div className="classification-content ">
+                                            <h4 className="recommendTitle">  {'自营 '+item.recommendTitle}</h4>
+                                            <span className=" integral">{item.recommendGift}</span>
+                                            <p className="price">{'￥'+item.recommendPrice}</p>
+                                            <p>{item.graphicOrigin+"    |"+item.graphicComment+'条评论'}</p>
+                                        </div>
+
+                                    </Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+
+
+                </div>
+
+        )
+    }
+}
+export default connect(
+    state=>state.classification,
+    actions
+)(Classification);
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,17 +2,13 @@ import {combineReducers} from "redux"
 import search from "./search";
 import home from "./home";
 import list from "./list";
-<<<<<<< HEAD
+
+import classification from './classification'
+import personal from "./personal";
 import detail from './details'
 
-let reducers=combineReducers({
-    search,home,list,detail
-=======
-import personal from "./personal";
-
-let reducers=combineReducers({
-    search,home,list,personal
->>>>>>> bf0443576ba4cf91d074e1c41bfa064d53f7d114
-});
+let reducers = combineReducers({
+    search, home, list, classification, detail, personal
+})
 
 export default reducers;
