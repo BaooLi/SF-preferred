42138c1ec399a51f6d4340aa62ff66c231b2904f
sunxiaodan123 <1679922628@qq.com>
2017-12-23 02:00:01 +0800

    '完善功能'

--- src/containers/Details/DetailFooter/index.js
+++ src/containers/Details/DetailFooter/index.js
@@ -8,29 +8,34 @@ import actions from '../../../store/actions/details'
 class DetailFooter extends Component {
     constructor() {
         super();
-        this.state = {goodNum: 0,cartflay:false}
+        this.state = {goodNum: 0, cartflay: false}
     }
 
     componentDidMount() {
         //如果用户已登录，查看此商品在购物车中的数量
         let username = this.props.username;
         let id = this.props.details.recommendID
-        username ? this.props.findCart(username, id) : null
+        if (username) {
+            this.props.findCart(username, id)
+            setInterval(() => {
+                this.setState({goodNum: this.props.carNum})
+            }, 300)
+        }
     }
 
     handleClick = () => {
-        let goodNum = this.props.num+this.state.goodNum;
+        let goodNum = this.props.num + this.state.goodNum;
         this.setState({goodNum});
         this.props.addCart({
-            username: this.props.username||"",
+            username: this.props.username || "",
             recommendID: this.props.details.recommendID,
             count: goodNum
         })
-        if(!this.state.cartflay){
-            this.setState({cartflay:true},()=>{
-                setTimeout(()=>{
-                    this.setState({cartflay:false});
-                },2000)
+        if (!this.state.cartflay) {
+            this.setState({cartflay: true}, () => {
+                setTimeout(() => {
+                    this.setState({cartflay: false});
+                }, 2000)
             });
         }
     }
@@ -47,14 +52,14 @@ class DetailFooter extends Component {
                     </i>
                 </a>
                 <a href="javascript:void(0);">
-                    {this.state.cartflay?(<h1><img src={this.props.img} alt=""/></h1>):null}
+                    {this.state.cartflay ? (<h1><img src={this.props.img} alt=""/></h1>) : null}
 
                     <span onClick={this.handleClick}>加入到购物车</span>
                 </a>
                 {
-                    this.props.username?<Link to={{pathname:`/cart`,state:this.props.username}}>
+                    this.props.username ? <Link to={{pathname: `/cart`, state: this.props.username}}>
                         <span>一键购买</span>
-                    </Link>:<Link to="/login">
+                    </Link> : <Link to="/login">
                         <span>一键购买</span>
                     </Link>
                 }
--- src/store/reducers/details.js
+++ src/store/reducers/details.js
@@ -17,7 +17,7 @@ export default function (state = initSearch, action) {
         case types.FIND_CART:
             return {...state, carNum: action.payload};
         case types.ADD_CART:
-            return {...state, carNum: state.carNum+action.payload};
+            return {...state, carNum: action.payload};
         default :
             return state;
     }
